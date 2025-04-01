67823b53ca0922abb59b2616531152775e93b1f2
Christopher Revell <christopher.revell@manchester.ac.uk>
2025-04-01 16:39:16 +0100

    Latest versions

--- src/IncidenceMatrixTools.jl
+++ src/IncidenceMatrixTools.jl
@@ -1,22 +1,21 @@
 
 module IncidenceMatrixTools
 
-using FromFile
+    using FromFile
 
-@from "MakeIncidenceMatrix.jl" using MakeIncidenceMatrix
-@from "MakeWeightMatrices.jl" using MakeWeightMatrices
+    @from "MakeIncidenceMatrix.jl" using MakeIncidenceMatrix
+    @from "MakeWeightMatrices.jl" using MakeWeightMatrices
 
-export ij_To_k
-export k_To_ij
-# export makeIncidenceMatrix2D
-export makeIncidenceMatrix3D
-# export solChecks
-export makeGhostVertexMask
-export makeGhostEdgeMask
-export vertexVolumeWeightsMatrix
-export vertexVolumeWeightsInverseMatrix
-export edgeLengthMatrix
-export edgeLengthInverseMatrix
-export setBoundaryEdgesToZero
+    export ij_To_k
+    export k_To_ij
+    export makeIncidenceMatrix3D
+    export makeGhostVertexMask
+    export makeGhostEdgeMask
+    export makeGhostEdgeMaskNew
+    export vertexVolumeWeightsMatrix
+    export vertexVolumeWeightsInverseMatrix
+    export edgeLengthMatrix
+    export edgeLengthInverseMatrix
+    export setBoundaryEdgesToZero
 
 end
--- src/MakeIncidenceMatrix.jl
+++ src/MakeIncidenceMatrix.jl
@@ -1,6 +1,6 @@
 #
 #  MakeIncidenceMatrix.jl
-#  GolgiModels
+#  IncidenceMatrixTools
 #
 #  Created by Christopher Revell on 02/05/2024.
 
@@ -8,6 +8,7 @@ module MakeIncidenceMatrix
 
 using LinearAlgebra
 using SparseArrays
+using Base.Threads
 
 # State matrix uMat has dimensions Nplus x Nplus. 
 # Column major order, so when flattened to a state vector u, u[1:Nplus] == uMat[:,1], u[1+Nplus:2*Nplus]==uMat[2,:] etc
@@ -26,108 +27,95 @@ function k_To_ij(k, N1)
     return (i,j)
 end
 
-# https://mbernste.github.io/posts/laplacian_matrix/
-# Make incidence matrix mapping grid points to directed edges connecting them 
-# Edges oriented from low index towards high index.
-# Edges labelled such that all i-directed edges form the first half of the edge list, in column major order. 
-function makeIncidenceMatrix2D(Nplus)
-
-    #      jj->
-    #    *---*---*
-    # ii \   \   \
-    # \  *---*---*
-    # v  \   \   \
-    #    *---*---*
- 
-    nVerts = Nplus*Nplus
-    edgesPerDirection = (Nplus-1)*Nplus
-    nEdges = 2*edgesPerDirection
-    dimEdges1 = Nplus-1
-    dimEdges2 = Nplus
-    A = spzeros(Int64, nEdges, nVerts)
-    # 1st dimension i directed Edges
-    for ii=1:dimEdges1
-        for jj=1:dimEdges2
-            j  = ij_To_k(ii, jj, dimEdges1)
-            k1 = ij_To_k(ii, jj, Nplus)
-            k2 = ij_To_k(ii + 1, jj, Nplus)
-            A[j, k1] = -1
-            A[j, k2] = 1
-        end
-    end 
-    # 2nd dimension j directed Edges
-    for ii=1:Nplus
-        for jj=1:dimEdges1
-            j  = ij_To_k(ii, jj, Nplus) + edgesPerDirection
-            k1 = ij_To_k(ii, jj, Nplus)
-            k2 = ij_To_k(ii, jj + 1, Nplus)
-            A[j, k1] = -1
-            A[j, k2] = 1
-        end
-    end 
-    return A
-end
-
-
 # A is a matrix of nVerts x nEdges
 # A[1:nEdgesi, :] corresponds to i directed edges
 # A[1:nEdgesj, :] corresponds to i directed edges
 # A[1:nEdgesk, :] corresponds to i directed edges
 # Vertices and edges in each direction ordered in 1D vector as per julia system data layout
-function makeIncidenceMatrix3D(ni, nj, nk)
+function makeIncidenceMatrix3D(dims)
+
+    if length(dims) == 3
+        ni, nj, nk = dims
+    elseif length(dims) == 2
+        ni, nj= dims
+        nk = 1
+    end
 
     nVerts = ni*nj*nk
     nEdgesi = (ni-1)*nj*nk
     nEdgesj = ni*(nj-1)*nk
     nEdgesk = ni*nj*(nk-1)
     nEdges = nEdgesi+nEdgesj+nEdgesk
-
-    A = spzeros(Int64, nEdges, nVerts)
-    vertArray = reshape(collect(1:nVerts), (ni, nj, nk))
-    iEdgeArray = reshape(collect(1:nEdgesi), (ni-1, nj, nk))
-    jEdgeArray = reshape(collect(1:nEdgesj), (ni, nj-1, nk))
-    kEdgeArray = reshape(collect(1:nEdgesk), (ni, nj, nk-1))
+    is = Int64[]
+    js = Int64[]
+    ks = Int64[]
+    vertArrayStrides = (1, ni, ni*nj)
 
     # Link i-directed edges to corresponding vertices
+    dimEdgeArrayStrides = (1, (ni-1), (ni-1)*nj)
     for kk=1:nk
         for jj=1:nj
             for ii=1:(ni-1)
-                edgeIndex = 1 + (ii-1)*stride(iEdgeArray,1) + (jj-1)*stride(iEdgeArray,2) + (kk-1)*stride(iEdgeArray,3)
-                v1 = 1 + (ii-1)*stride(vertArray,1) + (jj-1)*stride(vertArray,2) + (kk-1)*stride(vertArray,3)
-                v2 = 1 + (ii)*stride(vertArray,1) + (jj-1)*stride(vertArray,2) + (kk-1)*stride(vertArray,3)
-                A[edgeIndex,v1] = -1
-                A[edgeIndex,v2] = 1
+                edgeIndex = 1 + (ii-1)*dimEdgeArrayStrides[1] + (jj-1)*dimEdgeArrayStrides[2] + (kk-1)*dimEdgeArrayStrides[3]
+                v1 = 1 + (ii-1)*vertArrayStrides[1] + (jj-1)*vertArrayStrides[2] + (kk-1)*vertArrayStrides[3]
+                v2 = 1 + (ii)*vertArrayStrides[1] + (jj-1)*vertArrayStrides[2] + (kk-1)*vertArrayStrides[3]
+                # A[edgeIndex,v1] = -1
+                push!(is, edgeIndex)
+                push!(js, v1)
+                push!(ks, -1)
+                # A[edgeIndex,v2] = 1
+                push!(is, edgeIndex)
+                push!(js, v2)
+                push!(ks, 1)
             end
         end
     end  
     # Link j-directed edges to corresponding vertices
+    dimEdgeArrayStrides = (1, ni, ni*(nj-1))
     for kk=1:nk
         for jj=1:(nj-1)
             for ii=1:ni
-                edgeIndex = nEdgesi + 1 + (ii-1)*stride(jEdgeArray,1) + (jj-1)*stride(jEdgeArray,2) + (kk-1)*stride(jEdgeArray,3)
-                v1 = 1 + (ii-1)*stride(vertArray,1) + (jj-1)*stride(vertArray,2) + (kk-1)*stride(vertArray,3)
-                v2 = 1 + (ii-1)*stride(vertArray,1) + (jj)*stride(vertArray,2) + (kk-1)*stride(vertArray,3)
-                A[edgeIndex,v1] = -1
-                A[edgeIndex,v2] = 1
+                edgeIndex = nEdgesi + 1 + (ii-1)*dimEdgeArrayStrides[1] + (jj-1)*dimEdgeArrayStrides[2] + (kk-1)*dimEdgeArrayStrides[3]
+                v1 = 1 + (ii-1)*vertArrayStrides[1] + (jj-1)*vertArrayStrides[2] + (kk-1)*vertArrayStrides[3]
+                v2 = 1 + (ii-1)*vertArrayStrides[1] + (jj)*vertArrayStrides[2] + (kk-1)*vertArrayStrides[3]
+                # A[edgeIndex,v1] = -1
+                push!(is, edgeIndex)
+                push!(js, v1)
+                push!(ks, -1)
+                # A[edgeIndex,v2] = 1
+                push!(is, edgeIndex)
+                push!(js, v2)
+                push!(ks, 1)
             end
         end
     end  
     # Link k-directed edges to corresponding vertices
+    dimEdgeArrayStrides = (1, ni, ni*nj)
     for kk=1:(nk-1)
         for jj=1:nj
             for ii=1:ni
-                edgeIndex = nEdgesi + nEdgesj + 1 + (ii-1)*stride(kEdgeArray,1) + (jj-1)*stride(kEdgeArray,2) + (kk-1)*stride(kEdgeArray,3)
-                v1 = 1 + (ii-1)*stride(vertArray,1) + (jj-1)*stride(vertArray,2) + (kk-1)*stride(vertArray,3)
-                v2 = 1 + (ii-1)*stride(vertArray,1) + (jj-1)*stride(vertArray,2) + (kk)*stride(vertArray,3)
-                A[edgeIndex,v1] = -1
-                A[edgeIndex,v2] = 1
+                edgeIndex = nEdgesi + nEdgesj + 1 + (ii-1)*dimEdgeArrayStrides[1] + (jj-1)*dimEdgeArrayStrides[2] + (kk-1)*dimEdgeArrayStrides[3]
+                v1 = 1 + (ii-1)*vertArrayStrides[1] + (jj-1)*vertArrayStrides[2] + (kk-1)*vertArrayStrides[3]
+                v2 = 1 + (ii-1)*vertArrayStrides[1] + (jj-1)*vertArrayStrides[2] + (kk)*vertArrayStrides[3]
+                # A[edgeIndex,v1] = -1
+                push!(is, edgeIndex)
+                push!(js, v1)
+                push!(ks, -1)
+                # A[edgeIndex,v2] = 1
+                push!(is, edgeIndex)
+                push!(js, v2)
+                push!(ks, 1)
             end
         end
     end  
 
+    A = sparse(is, js, ks)
+
     return A
 end
 
-export ij_To_k, k_To_ij, makeIncidenceMatrix2D, makeIncidenceMatrix3D
+export ij_To_k
+export k_To_ij
+export makeIncidenceMatrix3D
 
 end
